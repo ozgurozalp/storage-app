7cf304da74611e17184e68ab55a5597b7491b137
gptmarker <gptmarker@gmail.com>
2022-12-08 13:16:34 +0300

    Update ListFolders.vue

--- src/components/dashboard/ListFolders.vue
+++ src/components/dashboard/ListFolders.vue
@@ -4,7 +4,7 @@
 	</div>
 	<div class="gap-2 flex flex-col" v-else-if="storage.folders && storage.folders.length > 0">
 		<h4 class="font-medium text-xl">Folders</h4>
-		<div class="grid grid-cols-4 sm:grid-cols-4 md:grid-cols-6 gap-3">
+		<div class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 gap-2">
 			<Folder is-root />
 			<Folder v-for="folder in storage.folders" :folder="folder" :key="folder._id" />
 		</div>
